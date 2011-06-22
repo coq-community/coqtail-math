1d90997a260fd4ed3bccec6becdb99b456389ca7
gallais <gallais@7194d10d-a3ed-4628-aea2-7b4a0dfde000>
2011-06-22 09:58:04 +0000

    Dequa_examples: now heavily relying on solve_diff_equa
    git-svn-id: svn+ssh://svn.code.sf.net/p/coqtail/code@290 7194d10d-a3ed-4628-aea2-7b4a0dfde000

--- Reals/Dequa_examples.v
+++ Reals/Dequa_examples.v
@@ -1,59 +1,54 @@
-Require Import Reals Rfunction_def Rfunction_facts Rextensionality.
 Require Import Rsequence_def Rsequence_base_facts.
+Require Import Reals Rfunction_def Rfunction_facts Rextensionality.
 
 Require Import Nth_derivative_def Nth_derivative_facts.
 Require Import Rpser.
 Require Import C_n_def C_n_facts C_n_usual.
 
-Require Import Dequa_def Dequa_facts.
+Require Import Dequa_def Dequa_facts Dequa_quote.
 Require Import List.
 
 Require Import LegacyField_Theory.
 
-Open Local Scope de_scope.
+Open Local Scope R_scope.
 
-Lemma constant_is_cst : forall r, 
- [| y 0 0 :=: `c r |]R ((existT _ _ (constant_infinite_cv_radius r)) :: nil).
+Lemma constant_is_cst : forall (c : R) (rc: infinite_cv_radius (constant_seq c)),
+  forall x, sum _ rc x = c.
 Proof.
-intros r ; apply interp_equa_in_N_R ; simpl ; apply An_nth_deriv_0.
+intros c rc ; solve_diff_equa ; reflexivity.
 Qed.
 
-Lemma identity_is_id :
- [| y 0 1 :=: cst 1 |]R ((existT _ _ identity_infinite_cv_radius) :: nil).
+Lemma identity_is_id : forall (ri : infinite_cv_radius identity_seq)
+ (di : derivable (sum _ ri)), forall x, derive (sum _ ri) di x = 1.
 Proof.
-apply interp_equa_in_N_R ; simpl ; rewrite An_nth_deriv_1.
-intro n ; unfold An_deriv, Rseq_shift, Rseq_mult, constant_seq, identity_seq.
- destruct (eq_nat_dec n 0) ; destruct (eq_nat_dec (S n) 1).
-   subst ; rewrite Rmult_1_r ; reflexivity.
-   subst ; destruct (f (eq_refl 1%nat)).
-  inversion e as [H] ; destruct (f H).
-  rewrite Rmult_0_r ; reflexivity.
+intros ri di ; solve_diff_equa ; unfold An_deriv, identity_seq,
+ constant_seq, Rseq_shift, Rseq_mult ; intros [ | n] ; simpl ; ring.
 Qed.
 
-Lemma diff_equa_cos :
- [| y 0 2 :=: - y 0 0 |]R ((existT _ _ cos_infinite_cv_radius) :: nil).
+Lemma diff_equa_cos : forall (rc : infinite_cv_radius cos_seq),
+  forall x, nth_derive (sum _ rc) (C_infty_Rpser _ rc 2%nat) x
+  = - nth_derive (sum _ rc) (C_infty_Rpser _ rc O) x.
 Proof.
-apply interp_equa_in_N_R ; simpl.
- do 2 (rewrite An_nth_deriv_S', An_nth_deriv_0).
- intro n ; rewrite (An_deriv_ext _ (- sin_seq)%Rseq) ;
-  [ | apply Deriv_cos_seq_simpl] ; rewrite An_deriv_opp_compat ;
-  unfold Rseq_opp ; apply Ropp_eq_compat ;
-  apply Deriv_sin_seq_simpl.
+intro rc ; solve_diff_equa ; intro n ; rewrite An_nth_deriv_S',
+ An_nth_deriv_1, (An_deriv_ext _ (- sin_seq)).
+ rewrite An_deriv_opp_compat ; unfold Rseq_opp ;
+  apply Ropp_eq_compat ; apply Deriv_sin_seq_simpl.
+ apply Deriv_cos_seq_simpl.
 Qed.
 
-Lemma diff_equa_sin :
- [| y 0 2 :=: - y 0 0 |]R ((existT _ _ sin_infinite_cv_radius) :: nil).
+Lemma diff_equa_sin : forall (rc : infinite_cv_radius sin_seq),
+  forall x, nth_derive (sum _ rc) (C_infty_Rpser _ rc 2%nat) x
+  = - nth_derive (sum _ rc) (C_infty_Rpser _ rc O) x.
 Proof.
-apply interp_equa_in_N_R ; simpl.
- do 2 (rewrite An_nth_deriv_S', An_nth_deriv_0).
- intro n ; rewrite (An_deriv_ext _ (cos_seq)%Rseq) ;
-  [apply Deriv_cos_seq_simpl | apply Deriv_sin_seq_simpl].
+intro rc ; solve_diff_equa ; intro n ; rewrite An_nth_deriv_S',
+ An_nth_deriv_1, (An_deriv_ext _ cos_seq) ;
+ [apply Deriv_cos_seq_simpl | apply Deriv_sin_seq_simpl].
 Qed.
 
-Lemma diff_equa_exp : forall n,
- [| y 0 (S n) :=: y 0 n |]R ((existT _ _ exp_infinite_cv_radius) :: nil).
+Lemma diff_equa_exp : forall n (re : infinite_cv_radius exp_seq), forall x,
+  nth_derive (sum _ re) (C_infty_Rpser _ re (S n)) x
+  = nth_derive (sum _ re) (C_infty_Rpser _ re n) x.
 Proof.
-intro n ; apply interp_equa_in_N_R ; simpl ;
- rewrite An_nth_deriv_S', (An_nth_deriv_ext _ exp_seq) ;
- [reflexivity | apply Deriv_exp_seq_simpl].
+intros n re ; solve_diff_equa ; rewrite An_nth_deriv_S',
+ (An_nth_deriv_ext _ exp_seq) ; [reflexivity | apply Deriv_exp_seq_simpl].
 Qed.
