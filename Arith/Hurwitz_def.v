8ad1a17a1dcd608c38b74a3f19943a95af715ed3
jmadiot <jmadiot@7194d10d-a3ed-4628-aea2-7b4a0dfde000>
2012-02-13 21:15:16 +0000

    La multiplication est associative, yay.
    git-svn-id: svn+ssh://svn.code.sf.net/p/coqtail/code@372 7194d10d-a3ed-4628-aea2-7b4a0dfde000

--- Arith/Hurwitz_def.v
+++ Arith/Hurwitz_def.v
@@ -19,3 +19,56 @@ Infix " h- " := hminus (at level 60).
 Ltac hastuce := unfold hminus, hopp, hadd ; simpl ; f_equal ; ring.
 
 
+(* for x in h i j k; do for y in h i j k; do
+     echo "let $x$y := $x h1 * $y h2 in"; done; done *)
+
+Definition hmul (h1 h2 : Hurwitz) : Hurwitz :=
+  let hh := h h1 * h h2 in
+  let hi := h h1 * i h2 in
+  let hj := h h1 * j h2 in
+  let hk := h h1 * k h2 in
+  let ih := i h1 * h h2 in
+  let ii := i h1 * i h2 in
+  let ij := i h1 * j h2 in
+  let ik := i h1 * k h2 in
+  let jh := j h1 * h h2 in
+  let ji := j h1 * i h2 in
+  let jj := j h1 * j h2 in
+  let jk := j h1 * k h2 in
+  let kh := k h1 * h h2 in
+  let ki := k h1 * i h2 in
+  let kj := k h1 * j h2 in
+  let kk := k h1 * k h2 in
+    mkHurwitz
+      (- hh - hi - hj - hk - ih - 2 * ii - jh - 2 * jj - kh - 2 * kk)
+      (hh + hi + hk + ih + ii + jh + jj + jk - kj + kk)
+      (hh + hi + hj + ii - ik + jh + jj + kh + ki + kk)
+      (hh + hj + hk + ih + ii + ij - ji + jj + kh + kk)
+    .
+
+Lemma rewh a b c d : h {| h := a; i := b; j := c; k := d |} = a.
+auto.
+Qed.
+
+Lemma rewi a b c d : i {| h := a; i := b; j := c; k := d |} = b.
+auto.
+Qed.
+
+Lemma rewj a b c d : j {| h := a; i := b; j := c; k := d |} = c.
+auto.
+Qed.
+
+Lemma rewk a b c d : k {| h := a; i := b; j := c; k := d |} = d.
+auto.
+Qed.
+
+Goal forall a b c, hmul a (hmul b c) = hmul (hmul a b) c.
+intros [] [] [].
+intros.
+unfold hmul.
+repeat rewrite rewh.
+repeat rewrite rewi.
+repeat rewrite rewj.
+repeat rewrite rewk.
+f_equal; ring.
+Qed.
