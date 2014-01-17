438448d097d6d794c22ba91046d94e587b503ba6
gallais <guillaume.allais@ens-lyon.org>
2014-01-17 19:27:08 +0000

    The new way to generate a makefile

--- generate_makefile.sh
+++ generate_makefile.sh
@@ -1,18 +1,1 @@
-#!/bin/sh
-
-#DIRS="Arith Complex Hierarchy Modulo mytheories Reals Sets Subseq Topology"
-GENERATED_MAKEFILE="Makefile"
-SRC_V="$(find . -name '*.v' | grep -v Raxioms)"
-
-COQDOCOPTIONS="-g --coqlib http:\/\/coq.inria.fr\/library\/"
-
-coq_makefile -R . Coqtail $DIRS $SRC_V -no-install -opt \
-  | grep -v "^.cd [A-Za-z0-9]* ; ..MAKE. all" \
-  | grep -v "^..cd [A-Za-z0-9]* ; ..MAKE. clean." \
-  | sed 's/mkdir/mkdir -p/' \
-  | sed "s/^COQDOCLIBS:=/COQDOCLIBS:= $COQDOCOPTIONS /" \
-  | sed 's/\(..COQDOC. .*html.*\)$/\1\n\tsh alter_doc.sh html/' \
-  > $GENERATED_MAKEFILE
-
-touch .depend
-
+coq_makefile -f Make -o Makefile
